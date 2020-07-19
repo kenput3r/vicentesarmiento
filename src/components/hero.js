d59f7c4eb08a16310fd724389ae93db7e6696ae3
Kenneth Lyerly <kenput3r@gmail.com>
2020-07-18 19:10:27 -0700

    revert to hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,23 +1,21 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import BackgroundImage from "gatsby-background-image"
-import LiveVideo from "./LiveVideo"
 
 const Hero = () => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     heroImage: file(relativePath: { eq: "vicente-sarmiento-16x9-saf.jpg" }) {
-  //       childImageSharp {
-  //         fluid(maxWidth: 2048, quality: 100) {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    query {
+      heroImage: file(relativePath: { eq: "vicente-sarmiento-16x9-saf.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 2048, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
-  // return <BackgroundImage className="HeroBackground" fluid={data.heroImage.childImageSharp.fluid} role="img" aria-label="Vicente Samrmiento" />
-  return <LiveVideo />
+  return <BackgroundImage className="HeroBackground" fluid={data.heroImage.childImageSharp.fluid} role="img" aria-label="Vicente Samrmiento" />
 }
 
 export default Hero
